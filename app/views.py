ea0a0926daf6cdfdf3e44bacd54e328deadf3b54
bilal <azbk2004@gmail.com>
2025-07-17 11:48:25 +0600

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Car
+
+
+def index_view(request):
+    cars = Car.objects.all()
+
+    return render(request, 'app/index.html', {'cars': cars})
+
+
+
